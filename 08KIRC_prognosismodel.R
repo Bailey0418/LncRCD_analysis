e89a60922ea4a0b355b46f478704bf52d9853f81
Bailey0418 <Bailey_bailin@163.com>
2026-04-09 20:33:12 +0800

    Update 08KIRC_prognosismodel.R

--- 08KIRC_prognosismodel.R
+++ 08KIRC_prognosismodel.R
@@ -1,12 +1,4 @@
-#####prognosis model#####
-#cox_data
-setwd("")
-cancer_survival <- read_tsv("KIRC_sur.txt")
-cancer_survival <- cancer_survival[,c(2:4)]
-names(cancer_survival) <- c("PATIENT","OS","OS.time")
-cancer_survival <- cancer_survival[!duplicated(cancer_survival), ]
-
-cancer_fpkm <- read.csv("TCGA_KIRC_lncRNA_tum_fpkm.csv",row.names = 1)
+setwd("D:/Project/03SCI/250710_LncRCD/Computational and Structural Biotechnology Journal/Supplementary/KIRC/Prognosis")
 a <- read.csv("E:/RCD/data/cancer/KIRC/Apoptosis.csv")
 b <- read.csv("E:/RCD/data/cancer/KIRC/Cuproptosis.csv")
 c <- read.csv("E:/RCD/data/cancer/KIRC/Necroptosis.csv")
@@ -14,133 +6,284 @@ d <- read.csv("E:/RCD/data/cancer/KIRC/Pyroptosis.csv")
 e <- rbind(a,b,c,d)
 lnc <- unique(e$lncRNA)
 rm(a,b,c,d,e)
-rcd_lnc_fpkm <- cancer_fpkm[lnc,]
-rcd_lnc_fpkm <- t(rcd_lnc_fpkm)
-rcd_lnc_fpkm <- as.data.frame(rcd_lnc_fpkm)
+
+library(data.table)
 library(dplyr)
-rcd_lnc_fpkm$PatientID <- sapply(rownames(rcd_lnc_fpkm), function(x) {
-  parts <- unlist(strsplit(x, "\\."))
-  paste(parts[1:3], collapse = "-")
-})
+library(stringr)
+library(tibble)
+clinical <- fread("D:/Project/03SCI/250710_LncRCD/Computational and Structural Biotechnology Journal/Supplementary/KIRC/Prognosis/TCGA-KIRC.clinical.tsv.gz", data.table = FALSE)
+survival <- fread("D:/Project/03SCI/250710_LncRCD/Computational and Structural Biotechnology Journal/Supplementary/KIRC/Prognosis/TCGA-KIRC.survival.tsv.gz", data.table = FALSE)
+fpkm <- read.csv("E:/RCD/data/cancer/KIRC/fpkm/TCGA_KIRC_lncRNA_tum_fpkm.csv", row.names = 1)
+colnames(fpkm) <- gsub("\\.", "-", colnames(fpkm))
 
-# 按病人ID聚合（取均值）
-rcd_lnc_fpkm_patient <- rcd_lnc_fpkm %>%
-  group_by(PatientID) %>%
-  summarise(across(everything(), mean))
+surv2 <- survival %>%
+  transmute(
+    sample = as.character(sample),
+    patient_id = as.character(`_PATIENT`),
+    time = as.numeric(`OS.time`),
+    status = as.numeric(OS)
+  ) %>%
+  filter(!is.na(sample), !is.na(patient_id), !is.na(time), !is.na(status)) %>%
+  filter(time > 0) %>%
+  distinct(patient_id, .keep_all = TRUE)
+write.csv(surv2,"surv.csv")
 
-# 设置行名为PatientID
-rcd_lnc_fpkm_patient_mat <- as.data.frame(rcd_lnc_fpkm_patient)
-rownames(rcd_lnc_fpkm_patient_mat) <- rcd_lnc_fpkm_patient_mat$PatientID
-rcd_lnc_fpkm_patient_mat$PatientID <- NULL
+clin2 <- clinical %>%
+  transmute(
+    sample = as.character(sample),
+    patient_id = as.character(submitter_id),
+    age = as.numeric(age_at_index.demographic),
+    gender = as.character(gender.demographic),
+    stage = as.character(ajcc_pathologic_stage.diagnoses),
+    T = as.character(ajcc_pathologic_t.diagnoses),
+    M = as.character(ajcc_pathologic_m.diagnoses),
+    N = as.character(ajcc_pathologic_n.diagnoses),
+    sample_type = as.character(sample_type.samples)
+  ) %>%
+  filter(!is.na(patient_id)) %>%
+  distinct(patient_id, .keep_all = TRUE)
+write.csv(clin2,"clin.csv")
 
-common_samples <- intersect(rownames(rcd_lnc_fpkm_patient_mat), cancer_survival$PATIENT)
-length(common_samples)
-expr_mat <- rcd_lnc_fpkm_patient_mat[common_samples,]
-survival_info <- cancer_survival %>%
-  dplyr::filter(PATIENT %in% common_samples) %>%
-  dplyr::arrange(match(PATIENT, common_samples))
-all.equal(rownames(expr_mat), survival_info$PATIENT)
+expr_mat <- as.matrix(fpkm)
+expr_mat <- t(expr_mat)
+expr_mat <- as.data.frame(expr_mat)
+expr_mat$sample <- rownames(expr_mat)
+rownames(expr_mat) <- NULL
+expr_mat$sample_type_code <- substr(expr_mat$sample, 14, 15)
+table(expr_mat$sample_type_code)
+expr_tumor <- expr_mat %>%
+  filter(sample_type_code == "01")
+expr_tumor$patient_id <- substr(expr_tumor$sample, 1, 12)
+expr_tumor <- expr_tumor %>%
+  distinct(patient_id, .keep_all = TRUE)
+meta_cols <- c("sample", "sample_type_code", "patient_id")
+expr_cols <- setdiff(colnames(expr_tumor), meta_cols)
 
-cox_data <- cbind(survival_info,expr_mat,stringsAsFactors = TRUE)
-cox_data$OS <- as.integer(cox_data$OS)
-cox_data$OS.time <- as.integer(cox_data$OS.time)
-write.csv(cox_data,"KIRC_cox_data.csv")
+dat_model <- expr_tumor %>%
+  inner_join(surv2, by = "patient_id") %>%
+  left_join(clin2 %>% select(-sample), by = "patient_id")
 
-#survival analysis
+if("sample.x" %in% colnames(dat_model)) {
+  dat_model$sample <- dat_model$sample.x}
+dat_model <- dat_model %>%
+  select(-any_of(c("sample.x", "sample.y")))
+
+dat_model <- dat_model %>%
+  filter(!is.na(time), !is.na(status), time > 0)
+write.table(dat_model,file = "D:/Project/03SCI/250710_LncRCD/Computational and Structural Biotechnology Journal/Supplementary/KIRC/Prognosis/KIRC_model_data_all.tsv",sep = "\t",quote = FALSE,row.names = FALSE)
+
 library(survival)
 library(survminer)
-pfilter <- 0.05
-cox_data <- cox_data[,-1]
-uniresult <- data.frame()#单因素cox回归结果
-for(i in colnames(cox_data[,3:ncol(cox_data)])){   
-  unicox <- coxph(Surv(time = OS.time, event = OS) ~ cox_data[,i], data = cox_data)  
-  unisum<- summary(unicox)   
-  pvalue <- round(unisum$coefficients[,5],3) 
-  if(pvalue<pfilter){ 
-    uniresult <- rbind(uniresult,
-                       cbind(gene=i,
-                             HR=unisum$coefficients[,2],
-                             L95CI=unisum$conf.int[,3],
-                             H95CI=unisum$conf.int[,4],
-                             pvalue=unisum$coefficients[,5]
-                       ))
-  }
-}   
-write.csv(uniresult,file = "KIRC_单因素COX分析结果.csv",row.names = F)
-
-unigene <- subset(cox_data,select = c("OS","OS.time",uniresult$gene))
-multicox <- coxph(Surv(time = OS.time,event = OS) ~ ., data = unigene) 
-multisum <- summary(multicox)
-gene <- colnames(unigene)[3:ncol(unigene)]
-HR <- multisum$coefficients[,2]
-L95CI <- multisum$conf.int[,3]
-H95CI <- multisum$conf.int[,4]
-pvalue <- multisum$coefficients[,5]
-multiresult <- data.frame(gene=gene,
-                          HR=HR,
-                          L95CI=L95CI,
-                          H95CI=H95CI,
-                          pvalue=pvalue)
-multiresult <- multiresult[multiresult$pvalue<0.05,]
-write.csv(multiresult,file = "KIRC_多因素COX分析结果.csv",row.names = F)
-
-#risk score
-cancer_multi_lnc <- cox_data[, c(1:2, which(colnames(cox_data) %in% multiresult$gene))]
-for(i in 1:nrow(cancer_multi_lnc)){#risk score计算和分组
-  cancer_multi_lnc[i,12] <- (-1.252852)*cancer_multi_lnc[i,3]+0.9136963*cancer_multi_lnc[i,4]+ 0.4826291*cancer_multi_lnc[i,5]+
-    (-0.9343253)*cancer_multi_lnc[i,6]+ 1.752132*cancer_multi_lnc[i,7]+ 0.9766936*cancer_multi_lnc[i,8]+
-    0.3591962*cancer_multi_lnc[i,9]+ 0.3718698*cancer_multi_lnc[i,10]+ 0.4897705*cancer_multi_lnc[i,11]
-}
-names(cancer_multi_lnc)[12] <- "risk_score"
-res.cut <- surv_cutpoint(cancer_multi_lnc, #数据集
-                         time = "OS.time", #生存时间
-                         event = "OS", #生存状态
-                         variables = "risk_score" #需要计算的数据列名
-)
-summary(res.cut)
-plot(res.cut, "risk_score", palette = "npg")
-res.cat <- surv_categorize(res.cut)
-head(res.cat)
-write.csv(res.cat,"riskscore_group.csv")
-cancer_multi_lnc$group <- res.cat$risk_score
-fit <- survfit(Surv(OS.time, OS) ~ group, data = cancer_multi_lnc)
-print(fit)
-ggsurvplot(fit,
-           pval = TRUE, conf.int = TRUE,
-           risk.table = TRUE, # 添加风险表
-           risk.table.col = "strata", # 根据分层更改风险表颜色
-           linetype = "strata", # 根据分层更改线型
-           surv.median.line = "hv", # 同时显示垂直和水平参考线
-           ggtheme = theme_bw(), # 更改ggplot2的主题
-           palette = c("#E7B800", "#2E9FDF"))#定义颜色
-
-#model ROC
-library(survivalROC)
-cancer_multi_lnc$OS.time=cancer_multi_lnc$OS.time/365
-par(mar= c(5,5,1,1),cex.lab=1.2,cex.axis= 1.2) #先设置一下图形的边界
-sROC=survivalROC(Stime=cancer_multi_lnc$OS.time, status=cancer_multi_lnc$OS, marker = cancer_multi_lnc$risk_score, predict.time =1, method="KM")#先画一个1年的图
-plot(sROC$FP, sROC$TP, type="l", xlim=c(0,1), ylim=c(0,1),col="red", 
-     xlab="False positive rate", ylab="True positive rate",
-     lwd = 2, cex.main=1.3, cex.lab=1.5, cex.axis=1.2, font=1.2)
-abline(0,1)
-aucText=paste0("1 years"," (AUC=",sprintf("%.3f",sROC$AUC),")") #这个后面添加legend用
-sROC3=survivalROC(Stime=cancer_multi_lnc$OS.time, status=cancer_multi_lnc$OS, marker = cancer_multi_lnc$risk_score, predict.time =3, method="KM")#再加一个3年的线
-lines(sROC3$FP, sROC3$TP, type="l", xlim=c(0,1), ylim=c(0,1),col="green",lwd = 2)
-aucText3=paste0("3 years"," (AUC=",sprintf("%.3f",sROC3$AUC),")") #这个后面添加legend用
-sROC5=survivalROC(Stime=cancer_multi_lnc$OS.time, status=cancer_multi_lnc$OS, marker = cancer_multi_lnc$risk_score, predict.time =5, method="KM")#再加一个5年的线
-lines(sROC5$FP, sROC5$TP, type="l", xlim=c(0,1), ylim=c(0,1),col="blue",lwd = 2)
-aucText5=paste0("5 years"," (AUC=",sprintf("%.3f",sROC5$AUC),")") #这个后面添加legend用
-legend("bottomright", c(aucText,aucText3,aucText5),lwd=2,bty="n",col=c("red","green","blue"),cex=1.2)#添加legend
-
+library(glmnet)
 library(timeROC)
-tROC <-timeROC(T=cancer_multi_lnc$OS.time,delta = cancer_multi_lnc$OS,marker = cancer_multi_lnc$risk_score,
-               cause = 1,times = c(1,3,5),ROC=T)
-par(mar= c(5,5,1,1),cex.lab=1.5,cex.axis= 1.2) #设置图形边界
-plot(tROC,time=1,col="red",title=F,lwd=2) #1年ROC
-plot(tROC,time=3,col="green",add=T,title=F,lwd=2) #3年ROC
-plot(tROC,time=5,col="blue",add=T,title=F,lwd=2) #5年ROC
-legend(0,1, # 这里设置legend的位置坐标：横坐标0,纵坐标1。也可以像前面一样设置为"bottomright"等
-       c(paste0("AUC at 1 years  ", round(tROC$AUC[1], 2)),
-         paste0("AUC at 3 years  ", round(tROC$AUC[2], 2)),
-         paste0("AUC at 5 years  ", round(tROC$AUC[3], 2))),
-       col=c("red","green","blue"),lwd=2,cex=1.2,bty="n")
+library(dplyr)
+library(tibble)
+library(ggplot2)
+library(pheatmap)
+library(rms)
+dat <- dat_model %>%
+  select(patient_id, sample, time, status, age, gender, stage, T, M, N, all_of(lnc))
+dat <- dat %>%
+  filter(!is.na(time), !is.na(status), time > 0)
+dat_clin <- dat %>%
+  filter(!is.na(age), !is.na(gender), !is.na(stage))
+#单因素cox
+set.seed(1234)
+train_id <- sample(1:nrow(dat), size = floor(nrow(dat) * 0.5))
+train <- dat[train_id, ]
+test  <- dat[-train_id, ]
+candidate_lnc <- lnc
+uni_cox <- lapply(candidate_lnc, function(gene) {
+  formula <- as.formula(paste0("Surv(time, status) ~ `", gene, "`"))
+  fit <- coxph(formula, data = train)
+  s <- summary(fit)
+  data.frame(
+    gene = gene,
+    HR = s$coefficients[,"exp(coef)"],
+    lower95 = s$conf.int[,"lower .95"],
+    upper95 = s$conf.int[,"upper .95"],
+    pvalue = s$coefficients[,"Pr(>|z|)"]
+  )
+})
+uni_cox_res <- do.call(rbind, uni_cox)
+uni_cox_res <- uni_cox_res[order(uni_cox_res$pvalue), ]
+uni_cox_res
+sig_lnc <- uni_cox_res$gene[uni_cox_res$pvalue < 0.05]
+sig_lnc
+write.csv(uni_cox_res,"uni_cox_res.csv")
+#LASSO
+x_train <- as.matrix(train[, sig_lnc, drop = FALSE])
+y_train <- Surv(train$time, train$status)
+set.seed(1234)
+cvfit <- cv.glmnet(
+  x_train,
+  y_train,
+  family = "cox",
+  alpha = 1,
+  nfolds = 10)
+plot(cvfit)
+fit <- glmnet(x_train, y_train, family = "cox", alpha = 1)
+plot(fit, xvar = "lambda")
+coef_min <- coef(cvfit, s = "lambda.min")
+active_index <- which(coef_min != 0)
+lasso_genes <- rownames(coef_min)[active_index]
+lasso_genes
+lasso_coef <- as.matrix(coef_min[active_index])
+lasso_coef
+#多因素cox
+final_formula <- as.formula(
+  paste("Surv(time, status) ~", paste(paste0("`", lasso_genes, "`"), collapse = " + ")))
+final_cox <- coxph(final_formula, data = train)
+summary(final_cox)
+final_coef <- summary(final_cox)$coefficients
+final_coef
+write.csv(final_coef,"final_coef.csv")
+#risk score
+train$riskScore <- predict(final_cox, type = "lp", newdata = train)
+test$riskScore <- predict(final_cox, type = "lp", newdata = test)
+dat$riskScore <- predict(final_cox, type = "lp", newdata = dat)
+cut_res <- surv_cutpoint(
+  data = train[, c("time", "status", "riskScore")],
+  time = "time",
+  event = "status",
+  variables = "riskScore"
+)
+cutoff <- cut_res$cutpoint[1, "cutpoint"]
+cutoff
+plot(cut_res, "riskScore", palette = "npg")
+
+train$riskGroup <- ifelse(train$riskScore > cutoff, "High", "Low")
+test$riskGroup  <- ifelse(test$riskScore > cutoff, "High", "Low")
+dat$riskGroup   <- ifelse(dat$riskScore > cutoff, "High", "Low")
+train$riskGroup <- factor(train$riskGroup, levels = c("Low", "High"))
+test$riskGroup  <- factor(test$riskGroup, levels = c("Low", "High"))
+dat$riskGroup   <- factor(dat$riskGroup, levels = c("Low", "High"))
+#KM
+fit_km_train <- survfit(Surv(time, status) ~ riskGroup, data = train)
+ggsurvplot(
+  fit_km_train,
+  data = train,
+  pval = TRUE, conf.int = TRUE,
+  risk.table = TRUE,
+  risk.table.col = "strata", # 根据分层更改风险表颜色
+  linetype = "strata", # 根据分层更改线型
+  surv.median.line = "hv", # 同时显示垂直和水平参考线
+  ggtheme = theme_bw(), # 更改ggplot2的主题
+  palette = c("#E7B800", "#2E9FDF"))#定义颜色
+
+fit_km_test <- survfit(Surv(time, status) ~ riskGroup, data = test)
+ggsurvplot(
+  fit_km_test,
+  data = test,
+  pval = TRUE, conf.int = TRUE,
+  risk.table = TRUE,
+  risk.table.col = "strata", # 根据分层更改风险表颜色
+  linetype = "strata", # 根据分层更改线型
+  surv.median.line = "hv", # 同时显示垂直和水平参考线
+  ggtheme = theme_bw(), # 更改ggplot2的主题
+  palette = c("#E7B800", "#2E9FDF"))#定义颜色
+#ROC
+roc_train <- timeROC(
+  T = train$time,
+  delta = train$status,
+  marker = train$riskScore,
+  cause = 1,
+  times = c(365, 1095, 1825),
+  iid = TRUE)
+plot(roc_train, time = 365, col = "red", title = FALSE)
+plot(roc_train, time = 1095, add = TRUE, col = "blue")
+plot(roc_train, time = 1825, add = TRUE, col = "green")
+legend("bottomright",
+       legend = c(
+         paste0("1-year AUC=", round(roc_train$AUC[1], 3)),
+         paste0("3-year AUC=", round(roc_train$AUC[2], 3)),
+         paste0("5-year AUC=", round(roc_train$AUC[3], 3))
+       ),
+       col = c("red", "blue", "green"),
+       lwd = 2)
+
+roc_test <- timeROC(
+  T = test$time,
+  delta = test$status,
+  marker = test$riskScore,
+  cause = 1,
+  times = c(365, 1095, 1825),
+  iid = TRUE)
+plot(roc_test, time = 365, col = "red", title = FALSE)
+plot(roc_test, time = 1095, add = TRUE, col = "blue")
+plot(roc_test, time = 1825, add = TRUE, col = "green")
+legend("bottomright",
+       legend = c(
+         paste0("1-year AUC=", round(roc_test$AUC[1], 3)),
+         paste0("3-year AUC=", round(roc_test$AUC[2], 3)),
+         paste0("5-year AUC=", round(roc_test$AUC[3], 3))
+       ),
+       col = c("red", "blue", "green"),
+       lwd = 2)
+#独立预后分析
+dat2 <- dat[,c("sample", "time", "status", "riskScore", "age", "gender", "stage")]
+dat2 <- as.data.frame(dat2)
+dat2$time <- as.numeric(dat2$time)
+dat2$status <- as.numeric(dat2$status)
+dat2$gender <- as.factor(dat2$gender)
+dat2$stage  <- as.factor(dat2$stage)
+dat$age <- as.numeric(dat$age)
+vars <- c("riskScore", "age", "gender", "stage")
+uni_res <- lapply(vars, function(v){
+  fml <- as.formula(paste0("Surv(time, status) ~ ", v))
+  fit <- coxph(fml, data = dat2)
+  s <- summary(fit)
+  
+  data.frame(
+    Variable = rownames(s$coefficients),
+    HR = s$coefficients[, "exp(coef)"],
+    lower95 = s$conf.int[, "lower .95"],
+    upper95 = s$conf.int[, "upper .95"],
+    pvalue = s$coefficients[, "Pr(>|z|)"],
+    row.names = NULL
+  )
+})
+uni_res <- do.call(rbind, uni_res)
+uni_res
+
+dat2$gender <- factor(dat2$gender)
+dat2$gender <- relevel(dat2$gender, ref = "female")
+dat2$stage <- factor(dat2$stage)
+dat2$stage <- relevel(dat2$stage, ref = "Stage I")
+multi_fit <- coxph(Surv(time, status) ~ riskScore + age + gender + stage, data = dat2)
+multi_sum <- summary(multi_fit)
+multi_res <- data.frame(
+  Variable = rownames(multi_sum$coefficients),
+  HR = multi_sum$coefficients[, "exp(coef)"],
+  lower95 = multi_sum$conf.int[, "lower .95"],
+  upper95 = multi_sum$conf.int[, "upper .95"],
+  pvalue = multi_sum$coefficients[, "Pr(>|z|)"],
+  row.names = NULL)
+write.csv(multi_res, "multivariate_cox_result_yuhou.csv", row.names = FALSE)
+multi_res$Variable <- gsub("^riskScore$", "Risk score", multi_res$Variable)
+multi_res$Variable <- gsub("^age$", "Age", multi_res$Variable)
+multi_res$Variable <- gsub("^gendermale$", "Male vs Female", multi_res$Variable)
+multi_res$Variable <- gsub("^stageStage II$", "Stage II vs Stage I", multi_res$Variable)
+multi_res$Variable <- gsub("^stageStage III$", "Stage III vs Stage I", multi_res$Variable)
+multi_res$Variable <- gsub("^stageStage IV$", "Stage IV vs Stage I", multi_res$Variable)
+#独立预后
+tabletext <- cbind(
+  c("Variable", multi_res$Variable),
+  c("HR (95% CI)", paste0(
+    round(multi_res$HR, 3), " (",
+    round(multi_res$lower95, 3), "-",
+    round(multi_res$upper95, 3), ")"
+  )),
+  c("P value", signif(multi_res$pvalue, 3)))
+library(forestplot)
+forestplot(
+  labeltext = tabletext,
+  mean  = c(NA, multi_res$HR),
+  lower = c(NA, multi_res$lower95),
+  upper = c(NA, multi_res$upper95),
+  zero = 1,
+  boxsize = 0.2,
+  line.margin = unit(8, "mm"),
+  col = fpColors(box = "#4575B4", line = "#4575B4", zero = "red"),
+  xlab = "Hazard Ratio",
+  title = "Multivariate Cox Regression")
